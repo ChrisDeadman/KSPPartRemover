38d8f60bd912b999122b484d741909e22ceff92f
deadman <christopher.hubmann@gmail.com>
2016-05-14 18:18:35 +0200

    Specify regular expression filter

---
+++ KSPPartRemover.Tests/Feature/RegexFilterTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using KSPPartRemover.Feature;
+
+namespace KSPPartRemover.Tests.Feature
+{
+    public class RegexFilterTest
+    {
+        [Test]
+        public void CanApplyRegularExpressionFilterOnCollection ()
+        {
+            // given
+            var target = new RegexFilter ("[Bb]ert");
+
+            var herbert = new {Name = "Herbert"};
+            var franz = new {Name = "Franz"};
+            var berthold = new {Name = "Berthold"};
+
+            // when
+            var filtered = target.Apply (new[] { herbert, franz, berthold }, user => user.Name);
+
+            // then
+            Assert.That (filtered, Is.EquivalentTo (new[] { herbert, berthold }));
+        }
+
+        [Test]
+        public void SupportsInverseMatching ()
+        {
+            // given
+            var target = new RegexFilter ("![Bb]ert");
+
+            // when / then
+            Assert.That (target.Matches ("Herbert"), Is.False);
+            Assert.That (target.Matches ("Berthold"), Is.False);
+            Assert.That (target.Matches ("Franz"), Is.True);
+        }
+    }
+}
